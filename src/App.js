38334a34c2b77408c24d43bf6b370604a40bae0c
Ramses Landero <27landero@gmail.com>
2022-06-25 01:45:26 -0500

    ok css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
-import freeCodeCampLogo from '../src/img/freecodecamp-logo.png'
+// import freeCodeCampLogo from '../src/img/freecodecamp-logo.png'
 import TodoList from './components/TodoList';
 
+/*<div className='freecodecamp-logo-container' >
+        <img src={freeCodeCampLogo} className='freecodecamp-logo'  alt='log de Freecodecamp'/>
+</div>*/
 
 function App() {
   return (
+    <>
     <div className="Todo-app">
-      <div className='freecodecamp-logo-container' >
-        <img src={freeCodeCampLogo} className='freecodecamp-logo'  alt='log de Freecodecamp'/>
-      </div>
+      <h1 className='todo-list-title tracking-in-expand' > TO DO LIST      </h1>
       
       <div className='main-todo-list' >
-        <h1>Mis tareas</h1>
+        <h1>My Tasks</h1>
             <TodoList/>
         
       </div>
 
-
     </div>
+    <footer className='footer' >Made with React by Ramses Landero</footer>
+    </>
   );
 }
 
